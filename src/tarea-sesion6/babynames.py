6c5c7cf1094ed336d01b0ff2930e879a5816f498
Juan Tuya Garcia <juantg@hotmail.es>
2018-05-01 15:22:17 -0500

    Initial Commit

--- src/tarea-sesion6/babynames.py
+++ src/tarea-sesion6/babynames.py
@@ -2,7 +2,8 @@
 # Copyright 2010 Google Inc.
 # Licensed under the Apache License, Version 2.0
 # http://www.apache.org/licenses/LICENSE-2.0
-
+#REPOSITORIO
+#https://github.com/juantg98/python-isil/
 
 import sys
 import re
